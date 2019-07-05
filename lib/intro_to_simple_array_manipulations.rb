966828f8a83498b0b9789592a098b00c0ff61c38
Luke Menard <lukemenard@gmail.com>
2019-07-05 01:23:48 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -44,6 +44,6 @@ def using_delete(instructors, string)
   instructors.delete(string)
 end
 
-def using_delete_at(famous_robots, integer)
+def using_delete_at(famous_robots, integer = 2)
   famous_robots.delete(integer)
 end
