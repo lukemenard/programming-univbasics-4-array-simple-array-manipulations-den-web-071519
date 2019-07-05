19a1f82d084b2e8b5c89179adfe119ddaf5d679f
Luke Menard <lukemenard@gmail.com>
2019-07-05 01:02:27 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -32,4 +32,8 @@ def using_insert(list_of_programming_languages, another_language)
   list_of_programming_languages.insert(4, another_language)
 end
 
-def using_uniq()
+def using_uniq(haircuts)
+  haircuts.uniq
+end
+
+def using_flatten()
